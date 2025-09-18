0be35cd9a960ac4f27595a2f1468eeedaf82adf0
shiran <shiranknafo1206@gmail.com>
2025-09-18 10:02:11 +0300

    animals dict

--- animals_dict.py
+++ animals_dict.py
@@ -2,18 +2,22 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 dict1={ }
-img=Image.open("pictures/elefant.png")
-img.show()
-img=Image.open("pictures/cat.jpg")
-img.show()
+img=Image.open("animal_pictures/elefant.png")
 dict1["e"]=img
+img=Image.open("animal_pictures/cat.jpg")
 dict1["c"]=img
-img=Image.open("pictures/lion.png")
+img=Image.open("animal_pictures/lion.png")
 dict1["l"]=img
-img=Image.open("pictures/dog.png")
+img=Image.open("animal_pictures/dog.png")
 dict1["d"]=img
-img=Image.open("pictures/monkey.jpg")
+img=Image.open("animal_pictures/monkey.jpg")
 dict1["m"]=img
-img=Image.open("pictures/bat.png")
+img=Image.open("animal_pictures/bat.png")
 dict1["b"]=img
-print(dict1)
+img=Image.open("animal_pictures/giraffe.png")
+dict1["g"]=img
+img=Image.open("animal_pictures/alpaca.png")
+dict1["a"]=img
+img=Image.open("animal_pictures/fish.png")
+dict1["f"]=img
+
