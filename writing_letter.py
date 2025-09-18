f435973c48d11ebb6283c64136952bca0d468068
maya-nachmann <maya.nachmann@gmail.com>
2025-09-18 13:38:49 +0300

    writing letter game random

--- writing_letter.py
+++ writing_letter.py
@@ -7,10 +7,10 @@ def get_image(letter):
     return consts.LETTERS_IMAGES_DICT[letter]
 
 def get_letter():
-    letter = random.choice(string.ascii_letters)
-    return letter
+    consts.curr_letter = random.choice(string.ascii_letters)
+    return consts.curr_letter
 
-def compare_images():
-    letter_image = get_image()
+def compare_images(letter):
+    letter_image = get_image(letter)
     letter_drawing = 'image.png'
     return image_coparison.compare_two_images(letter_image, letter_drawing)
