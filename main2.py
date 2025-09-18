d844aa4fa6b6acfdadba5482f4326a9485624d8e
lizab <lizabodin2006official@gmail.com>
2025-09-18 16:45:22 +0300

    fp,urho

--- main2.py
+++ main2.py
@@ -46,14 +46,102 @@ def myWindow1():
         else:
             messagebox.showinfo("בוז!", "בוז!")
 
+    def play_game():
+        main_sound, main_letter, main_options = sound_letter.choose_sound()
+
+        root = Tk()
+
+        root.minsize(consts.ROOT_HEIGHT, consts.ROOT_WIDTH)
+        root.maxsize(consts.ROOT_HEIGHT, consts.ROOT_WIDTH)
+
+        sound_frame = Frame(root)
+        sound_frame.pack(side=TOP, padx=50, pady=50)
+
+        answer_frame = Frame(root)
+        answer_frame.pack(side=TOP, padx=50, pady=50)
+
+        pygame.mixer.init()
+
+        def start():
+            temp = main_options.copy()
+            for i in range(4):
+                letter = random.choice(temp)
+                temp.remove(letter)
+                if i == 0:
+                    btn_text1.set(letter)
+                elif i == 1:
+                    btn_text2.set(letter)
+                elif i == 2:
+                    btn_text3.set(letter)
+                else:
+                    btn_text4.set(letter)
+
+        def play_sound():
+            pygame.mixer.music.load(main_sound)
+            pygame.mixer.music.play(loops=0)
+
+        def check_answer(text):
+            if text == main_letter:
+                tkinter.messagebox.showinfo("כל הכבוד!", "תשובה נכונה")  # ok
+            else:
+                tkinter.messagebox.showinfo("איזה באסה", f"תשובה לא נכונה. התשובה הנכונה:\n {sound_letter.LETTER}")  # ok
+
+        def answer_1(event):
+            check_answer(btn_text1.get())
+
+        def answer_2(event):
+            check_answer(btn_text2.get())
+
+        def answer_3(event):
+            check_answer(btn_text3.get())
+
+        def answer_4(event):
+            check_answer(btn_text4.get())
+
+        btn_start = Button(sound_frame, text=":שחק", command=start)
+        btn_start.config(font=("Courier", 15), fg="black", bg="green")
+        btn_start.pack(side=TOP)
+
+        sound_label = Label(sound_frame, text=" :איזה אות עושה את הצליל ")
+        sound_label.config(font=("Courier", 25))
+        sound_label.pack(side=TOP)
+        sound_label.pack(side=RIGHT)
+        play_button = Button(sound_frame, text="הפעל", command=play_sound)
+        play_button.config(font=("Courier", 25), fg="black", bg="yellow")
+        play_button.pack(side=RIGHT)
+
+        btn_text1 = StringVar()
+        letter1 = Button(answer_frame, textvariable=btn_text1)
+        letter1.bind("<Button-1>", answer_1)
+        letter1.config(font=("Courier", 40), fg="black", bg="lightblue")
+        letter1.grid(row=0, column=0)
+
+        btn_text2 = StringVar()
+        letter2 = Button(answer_frame, textvariable=btn_text2)
+        letter2.bind("<Button-1>", answer_2)
+        letter2.config(font=("Courier", 40), fg="black", bg="lightblue")
+        letter2.grid(row=0, column=1)
+
+        btn_text3 = StringVar()
+        letter3 = Button(answer_frame, textvariable=btn_text3)
+        letter3.bind("<Button-1>", answer_3)
+        letter3.config(font=("Courier", 40), fg="black", bg="lightblue")
+        letter3.grid(row=0, column=2)
+
+        btn_text4 = StringVar()
+        letter4 = Button(answer_frame, textvariable=btn_text4)
+        letter4.bind("<Button-1>", answer_4)
+        letter4.config(font=("Courier", 40), fg="black", bg="lightblue")
+        letter4.grid(row=0, column=3)
+
+
+
     button1 = Button(window, text='כתיבה', command = writing_class, bg='brown', fg='white',
                              font=('helvetica', 9, 'bold'))
     canvas1.create_window(200, 180, window=button1)
-    button2 = Button(window, text='שמע',command= sound_letter.play_game, bg='brown', fg='white',
+    button2 = Button(window, text='שמע',command= play_game, bg='brown', fg='white',
                     font=('helvetica', 9, 'bold'))
     canvas1.create_window(200, 280, window=button2)
-
-
 
     window.mainloop()
 
